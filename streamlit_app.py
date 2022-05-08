8b3362ffd8099b7d01efabb583f968c06e7147e1
sahneyoghurt <sahneyoghurt@googlemail.com>
2022-05-08 15:14:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 #Create a repeatable code block called a function
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
     # take the json version of the reposnse and normalize it
     fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
     #output it to the screen as a table
